42c6c9bad64b566efb97c1ff877eecfb852c903d
Keiichiro Ono <keiono@gmail.com>
2018-05-07 15:42:13 -0700

    Dependencies updated.

--- frontend/src/components/TopPage/SourceSelector.jsx
+++ frontend/src/components/TopPage/SourceSelector.jsx
@@ -1,17 +1,30 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import TextField from 'material-ui/TextField'
-import MenuItem from 'material-ui/Menu/MenuItem'
 import Button from 'material-ui/Button'
 
-import { browserHistory } from 'react-router'
+import Menu, { MenuItem } from 'material-ui/Menu'
+
+
+import {browserHistory} from 'react-router'
 
 import ErrorDialog from './ErrorDialog'
 
 
 const textFieldStyle = {
   width: 400,
-  fontSize: '2em'
+  fontSize: '2em',
+}
+
+const textFieldStyleSmall = {
+  width: 300,
+  fontSize: '2em',
+  marginRight: '0.5em'
+}
+
+const startStyle = {
+  width: 400,
+  marginTop: '2em'
 }
 
 const SMALL_NAME = 'Fanconi Anemia gene ontology (FanGO)'
@@ -20,7 +33,7 @@ const EXAMPLE_UUIDS = {
   [SMALL_NAME]: 'c84ec0b0-02f4-11e8-bd69-0660b7976219',
   'Large hierarchy': '7ae8907a-b395-11e7-b629-0660b7976219',
   'DNA Repair': 'ab704ae4-0719-11e8-b03c-0660b7976219',
-  '80K': '77c476e6-2bb1-11e8-84e4-0660b7976219'
+  '80K': '77c476e6-2bb1-11e8-84e4-0660b7976219',
 }
 
 class SourceSelector extends Component {
@@ -30,26 +43,32 @@ class SourceSelector extends Component {
       uuid: '',
       serverUrl: props.dataSource.get('serverUrl'),
       example: EXAMPLE_UUIDS[SMALL_NAME],
-      openError: false
+      openError: false,
+      anchorEl: null,
+      selectedIndex: 1
     }
   }
 
   handleUuidChange = event => {
     this.setState({
-      uuid: event.target.value
+      uuid: event.target.value,
     })
   }
 
   handleUrlChange = event => {
     this.setState({
-      url: event.target.value
+      url: event.target.value,
     })
   }
 
-  handleExampleChange = event => {
+  handleExampleChange = (event, idx) => {
+    const uuid = Object.values(EXAMPLE_UUIDS)[idx]
+    this.setState({ anchorEl: null });
+
     this.setState({
-      example: event.target.value,
-      uuid: event.target.value
+      example: uuid,
+      uuid: uuid,
+      selectedIndex: idx
     })
   }
 
@@ -64,18 +83,28 @@ class SourceSelector extends Component {
     browserHistory.push('/app')
   }
 
+  handleExample = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   openErrorDialog = openError => {
     this.setState({
-      openError
+      openError,
     })
   }
 
   render() {
     const examples = Object.keys(EXAMPLE_UUIDS)
+    const { anchorEl } = this.state;
+
 
     return (
       <div>
-        <div style={{ width: '350px' }}>
+        <div style={{width: '450px'}}>
           <TextField
             style={textFieldStyle}
             placeholder="e.g. http://test.ndexbio.org"
@@ -85,33 +114,47 @@ class SourceSelector extends Component {
             onChange={this.handleUrlChange}
           />
 
-          <TextField
-            autoFocus={true}
-            required={true}
-            style={textFieldStyle}
-            label="UUID of the main hierarchy"
-            value={this.state.uuid}
-            margin="normal"
-            onChange={this.handleUuidChange}
-          />
+          <div>
+            <TextField
+              autoFocus={true}
+              style={textFieldStyleSmall}
+              required={true}
+              label="UUID of the main hierarchy"
+              value={this.state.uuid}
+              margin="normal"
+              onChange={this.handleUuidChange}
+            />
 
-          <TextField
-            id="examples"
-            select
-            label="Example Hierarchies:"
-            style={textFieldStyle}
-            value={this.state.example}
-            onChange={this.handleExampleChange}
-            margin="normal"
-          >
-            {examples.map(option => (
-              <MenuItem key={option} value={EXAMPLE_UUIDS[option]}>
-                {option}
-              </MenuItem>
-            ))}
-          </TextField>
+            <Button
+              aria-owns={anchorEl ? 'examples' : null}
+              aria-haspopup="true"
+              variant="raised" onClick={this.handleExample}>
+              Examples
+            </Button>
+            <Menu
+              id="examples"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleExampleChange}
+            >
+              {examples.map((option, index) => (
+                <MenuItem
+                  onClick={this.handleClose}
+                  key={option}
+                  selected={index === this.state.selectedIndex}
+                  onClick={event => this.handleExampleChange(event, index)}
+                  value={EXAMPLE_UUIDS[option]}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
 
-          <Button variant="raised" onClick={this.handleStart}>
+
+          <Button
+            color="primary"
+            style={startStyle}
+            variant="raised" onClick={this.handleStart}>
             Start
           </Button>
         </div>
