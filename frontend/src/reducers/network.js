c4e38556a93d88ed660997e6ecd9e085993d97ab
Keiichiro Ono <keiono@gmail.com>
2018-06-20 10:57:00 -0700

    README updated.

--- frontend/src/reducers/network.js
+++ frontend/src/reducers/network.js
@@ -1,18 +1,19 @@
-import {FETCH_NETWORK, RECEIVE_NETWORK, DELETE_NETWORK} from '../actions/network'
-import {Map} from 'immutable'
+import {
+  FETCH_NETWORK,
+  RECEIVE_NETWORK,
+  DELETE_NETWORK
+} from '../actions/network'
+import { Map } from 'immutable'
 
 const defState = Map({
   loading: false,
   index: null,
   idx2: null,
   error: null
-});
-
+})
 
 export default function networkState(state = defState, action) {
-
   switch (action.type) {
-
     case FETCH_NETWORK:
       return state.set('loading', true)
 
