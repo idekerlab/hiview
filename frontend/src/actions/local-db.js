15cd1cffcf46df910cc2f6b48b739313275d3a19
Keiichiro Ono <keiono@gmail.com>
2020-05-18 13:18:16 -0700

    Database name updated

--- frontend/src/actions/local-db.js
+++ frontend/src/actions/local-db.js
@@ -1,8 +1,8 @@
 // For local cache
 import Dexie from 'dexie'
 
-const DB_NAME = 'HiView'
-const DB_VERSION = 2
+const DB_NAME = 'HiView2'
+const DB_VERSION = 1
 const DB_STORE = 'hierarchy'
 const DB_PRIMARY_KEY = 'uuid'
 
