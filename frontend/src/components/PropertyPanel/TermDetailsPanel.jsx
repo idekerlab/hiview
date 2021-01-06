347e699446508f5f32488e35360db7f9aea2dd09
Sophie Liu <sol015@ucsd.edu>
2021-01-06 16:48:53 -0700

    Fixing merge mistake

--- frontend/src/components/PropertyPanel/TermDetailsPanel.jsx
+++ frontend/src/components/PropertyPanel/TermDetailsPanel.jsx
@@ -216,12 +216,8 @@ const TermDetailsPanel = props => {
             enrichmentActions={props.enrichmentActions}
             uiState={props.uiState}
             hierarchy={props.network.get('hierarchy')}
-<<<<<<< HEAD
             networkAreaStyle={{ height: '100%', background: '#333333' }}
             setCy={setCy}
-=======
-            networkAreaStyle={{ height: '100%', background: '#000000' }}
->>>>>>> 768718b57cf56e6f0e179423f9143fa30e91414a
           />
         </div>
       )
@@ -376,7 +372,7 @@ const TermDetailsPanel = props => {
       </div>
 
       <div className={classes.bottomPane}>
-        <CopyToClipboardButton geneList={geneList}/>
+        <CopyToClipboardButton geneList={geneList} />
         <LayoutSelector commandActions={props.interactionsCommandActions} />
         <AdvancedOptions networkProps={networkProps} raw={raw} geneList={geneList} {...props} cy={cy} />
         <div className={classes.control}>
