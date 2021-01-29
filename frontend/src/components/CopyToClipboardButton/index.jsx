033c8122e502a0fc7c92fcb85bb7d8a13bb43513
Keiichiro Ono <keiono@gmail.com>
2021-01-29 15:13:09 -0800

    Notification added for copy gene list function

--- frontend/src/components/CopyToClipboardButton/index.jsx
+++ frontend/src/components/CopyToClipboardButton/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createStyles, makeStyles } from '@material-ui/core'
 
 import Button from '@material-ui/core/Button'
 import ClipboardIcon from '@material-ui/icons/Assignment'
-
 import Tooltip from '@material-ui/core/Tooltip'
+
+import Notification from './Notification'
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -16,7 +17,7 @@ const useStyles = makeStyles(theme =>
       fontSize: '2em',
     },
     tooltip: {
-      fontSize: '16px',
+      fontSize: '1.2em',
       fontWeight: '300',
       textAlign: 'center',
     },
@@ -25,6 +26,8 @@ const useStyles = makeStyles(theme =>
 
 const CopyToClipboardButton = props => {
   const { genes } = props
+  const [open, setOpen] = useState(false)
+
   const classes = useStyles()
 
   const _handleCopy = () => {
@@ -32,18 +35,22 @@ const CopyToClipboardButton = props => {
     // console.log('Genes:', geneString)
     if (navigator.clipboard) {
       navigator.clipboard.writeText(geneString)
+      setOpen(true)
     }
   }
 
   return (
-    <Tooltip
-      title={<div className={classes.tooltip}>Copy genes above as space-delimited text to the clipboard</div>}
-      placement="bottom"
-    >
-      <Button size="small" variant="outlined" color="primary" onClick={_handleCopy} className={classes.button}>
-        <ClipboardIcon className={classes.icon} alt="Copy gene list to clipboard" />
-      </Button>
-    </Tooltip>
+    <React.Fragment>
+      <Tooltip
+        title={<div className={classes.tooltip}>Copy gene list as text</div>}
+        placement="bottom"
+      >
+        <Button size="small" variant="outlined" color="primary" onClick={_handleCopy} className={classes.button}>
+          <ClipboardIcon className={classes.icon} alt="Copy genes as text" />
+        </Button>
+      </Tooltip>
+      <Notification count={genes.length} setOpen={setOpen} open={open} />
+    </React.Fragment>
   )
 }
 
