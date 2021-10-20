ddeb73d8aed7e2a8ffe77a8a401772b6580ef5ff
Keiichiro Ono <keiono@gmail.com>
2021-10-20 01:35:39 -0700

    Extra linkout buttons added for genes

--- frontend/src/components/PropertyPanel/TitleBar.jsx
+++ frontend/src/components/PropertyPanel/TitleBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import OpenIcon from '@material-ui/icons/OpenInNew'
-import Typography from '@material-ui/core/Typography'
+import { Typography, Button } from '@material-ui/core'
 
 import { createStyles, makeStyles } from '@material-ui/core'
 
@@ -10,21 +10,41 @@ const useStyles = makeStyles(theme =>
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'flex-start',
+      flexDirection: 'column',
       boxSizing: 'border-box',
       margin: 0,
-      padding: '1em',
+      padding: theme.spacing(1),
+      paddingLeft: theme.spacing(2),
       width: '100%',
     },
     linkIcon: {
-      marginRight: '0.6em',
+      marginRight: theme.spacing(1),
       color: '#222222',
       '&:hover': {
         color: 'dodgerblue',
       },
     },
+    buttons: {
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginBottom: theme.spacing(0.5),
+      textTransform: 'none'
+
+    },
+    button: {
+      marginRight: theme.spacing(1),
+    },
+    title: {
+      display: 'flex',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    },
     geneSymbol: {
       color: '#333333',
-      marginRight: '0.7em',
+      marginRight: theme.spacing(2),
     },
   }),
 )
@@ -34,17 +54,58 @@ const TitleBar = props => {
 
   return (
     <div className={classes.container}>
-      <OpenIcon className={classes.linkIcon} onClick={handleClick(props.geneSymbol)} />
-      <Typography variant="h4" className={classes.geneSymbol}>
-        {props.geneSymbol}:
-      </Typography>
-      <Typography variant="h5">{props.title}</Typography>
+      <div className={classes.buttons}>
+        <Typography variant="h4" className={classes.geneSymbol}>
+          {props.geneSymbol}
+        </Typography>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          className={classes.button}
+          startIcon={<OpenIcon />}
+          onClick={_handleGeneCardsClick(props.geneSymbol)}
+        >
+          GeneCards
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          className={classes.button}
+          startIcon={<OpenIcon />}
+          onClick={_handlePdbClick(props.geneSymbol)}
+        >
+          PDB
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
+          className={classes.button}
+          startIcon={<OpenIcon />}
+          onClick={_handleAlphaClick(props.geneSymbol)}
+        >
+          AlphaFold
+        </Button>
+      </div>
+      <div className={classes.title}>
+        <Typography variant="h6">{props.title}</Typography>
+      </div>
     </div>
   )
 }
 
-const handleClick = gene => () => {
+const _handleGeneCardsClick = gene => () => {
   window.open(`http://www.genecards.org/cgi-bin/carddisp.pl?gene=${gene}`)
+}
+
+const _handlePdbClick = gene => () => {
+  window.open(`https://www.ebi.ac.uk/pdbe/entry/search/index?view=macromolecules&gene_name:${gene}`)
+}
+
+const _handleAlphaClick = gene => () => {
+  window.open(`https://alphafold.ebi.ac.uk/search/text/${gene}?organismScientificName=Homo%20sapiens`)
 }
 
 export default TitleBar
