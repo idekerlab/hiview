9562fa1548d6219f434596b49560cd2e3624c42a
Keiichiro Ono <keiono@gmail.com>
2022-10-07 12:40:40 -0700

    UD-2410 Tooltip for node style selecror added

--- frontend/src/components/PropertyPanel/NodeStyleSelector/index.jsx
+++ frontend/src/components/PropertyPanel/NodeStyleSelector/index.jsx
@@ -6,6 +6,7 @@ import {
   RadioGroup,
   FormControlLabel,
   Typography,
+  Tooltip,
 } from '@material-ui/core'
 
 import { makeStyles } from '@material-ui/core/styles'
@@ -30,7 +31,16 @@ const useStyles = makeStyles((theme) => ({
     padding: 0,
     margin: 0,
   },
+  row: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 }))
+
+const TOOLTIIP_TEXT = {
+  [NODE_STYLE.CURATION]:
+    'Proteins are colored orange when found in systematic lists describing DNA damage repair',
+}
 
 const NodeStyleSelector = ({ uiState, uiStateActions }) => {
   const classes = useStyles()
@@ -40,6 +50,16 @@ const NodeStyleSelector = ({ uiState, uiStateActions }) => {
     const newSelection = event.target.value
     uiStateActions.setNodeStyle(newSelection)
   }
+
+  const getFormControlLabel = (styleName) => (
+    <FormControlLabel
+      className={classes.label}
+      key={styleName}
+      value={NODE_STYLE[styleName]}
+      control={<Radio />}
+      label={<Typography variant={'body1'}>{NODE_STYLE[styleName]}</Typography>}
+    />
+  )
 
   return (
     <FormControl component="fieldset" className={classes.root}>
@@ -52,17 +72,24 @@ const NodeStyleSelector = ({ uiState, uiStateActions }) => {
         value={selection}
         onChange={handleChange}
       >
-        {Object.keys(NODE_STYLE).map((styleName) => (
-          <FormControlLabel
-            className={classes.label}
-            key={styleName}
-            value={NODE_STYLE[styleName]}
-            control={<Radio />}
-            label={
-              <Typography variant={'body1'}>{NODE_STYLE[styleName]}</Typography>
-            }
-          />
-        ))}
+        {Object.keys(NODE_STYLE).map((styleName) => {
+          const tooltip = TOOLTIIP_TEXT[NODE_STYLE[styleName]]
+
+          if (tooltip !== undefined) {
+            return (
+              <div className={classes.row} key={styleName}>
+                <Tooltip
+                  title={<Typography variant="body1">{tooltip}</Typography>}
+                  arrow
+                >
+                  {getFormControlLabel(styleName)}
+                </Tooltip>
+              </div>
+            )
+          } else {
+            return getFormControlLabel(styleName)
+          }
+        })}
       </RadioGroup>
     </FormControl>
   )
